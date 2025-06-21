bc3759e96eb5c33057b510a3f0b770c36a7ee852
kishan <dakakishan@gmail.com>
2025-06-21 19:12:56 +0530

    feat: version upgrade 0.80

--- template/src/config/Config.ts
+++ template/src/config/Config.ts
@@ -1,7 +1,10 @@
 import {Config as AppConfig} from 'react-native-config'
 
+const isStaging = true
+
 const Config = {
-  BASE_URL: AppConfig.BASE_URL
+  BASE_URL: isStaging ? AppConfig.BASE_URL : AppConfig.PROD_URL,
+  isPROD : !isStaging
 }
 
 export default Config
